583c088e33e07cc16323cbf6b29edaedcc01c749
LCode Technologies <lcodetechnologies@lcodes-macbook-air.local>
2018-11-14 12:23:32 +0530

    Initial Commit

---
+++ TestGithub1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGithub1
+//
+//  Created by LCode Technologies on 14/11/18.
+//  Copyright © 2018 Lcodetechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
